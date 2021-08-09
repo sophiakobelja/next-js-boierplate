4f5302bd0be5e5defc35fba394b4a87003761649
Sophia Kobelja <sophia.kobelja@jhuapl.edu>
2021-08-09 09:20:33 -0400

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import '../styles/globals.css';
-import '../styles/tachyons.css';
+import '@styles/globals.css';
+import '@styles/tachyons.css';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from 'src/store';
